ac2932e5f1dd7d899f9f43e53b0a79c4031ede6c
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-05-29 01:03:07 +0300

    Article class Object Relationship Methods established

--- lib/Author.py
+++ lib/Author.py
@@ -5,4 +5,16 @@ class Author:
     @property
     def name(self):
         return self._name
+    
+class Article:
+    def __init__(self, author, magazine, title):
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+    def author(self):
+        return self._author
+
+    def magazine(self):
+        return self._magazine
     pass
