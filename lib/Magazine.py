01fa962b5e2e50a7946962e362d3d3bdd92c6056
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-05-28 22:54:37 +0300

    Magazine class initialized. Class completed with Readers and Writers

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,2 +1,18 @@
 class Magazine:
+    instances = []
+
+    def __init__(self, name, category):
+        self._name = name
+        self._category = category
+        Magazine.instances.append(self)
+
+    def name(self):
+        return self._name
+
+    def category(self):
+        return self._category
+
+    @classmethod
+    def all(cls):
+        return cls.instances
     pass
