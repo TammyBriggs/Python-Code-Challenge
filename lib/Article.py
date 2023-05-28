b800dcfc8dabf62ab642d361dc804cc65cad05f1
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-05-28 22:54:57 +0300

    Article class initialized. Class completed with Readers and Writers

--- lib/Article.py
+++ lib/Article.py
@@ -1,2 +1,16 @@
 class Article:
+    instances = []
+
+    def __init__(self, author, magazine, title):
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article.instances.append(self)
+
+    def title(self):
+        return self._title
+
+    @classmethod
+    def all(cls):
+        return cls.instances
     pass
